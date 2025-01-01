e2d7274e9ce8fc38ea80725df0e3c55947e270f6
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2025-01-01 16:19:27 +0000

    o1_BasicReactAPP

---
+++ 01_basic-react-app/src/App.js
@@ -0,0 +1,14 @@
+import Extra from "./extra";
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <p>Utsav Vachhani</p>
+    </div>
+    <Extra/>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basic-react-app/src/extra.js
@@ -0,0 +1,7 @@
+function Extra() {
+    return(
+        <h1>I will Call You From the extra.js File</h1>
+    );
+}
+
+export default Extra;
