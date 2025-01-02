19619c366ab8f10ca8dfb3c56ae9950edafe1e62
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2025-01-02 12:42:57 +0000

    01_basic-react-app

---
+++ 01_basic-vit-app/src/App.jsx
@@ -0,0 +1,20 @@
+import Extra from './Extra';
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>
+        <p>Utsav Vachhani Vite Project</p>
+      </h1>
+
+
+      <Extra/>
+    </>
+  );
+}
+
+
+export default App;
---
+++ 01_basic-vit-app/src/Extra.jsx
@@ -0,0 +1,8 @@
+function Extra() {
+    return(
+        <h1>I will Call You From the extra.js File</h1>
+    );
+}
+
+
+export default Extra;
